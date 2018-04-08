c411f4e771e87562148d182241932d1a1dc6b2b2
Mike Apted <mikapted@amazon.com>
2018-04-08 15:50:21 -0400

    Bin cmd update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var chkip = require('chkip');
 cmd.version('0.1.0')
   .description('Command line interface for retrieving current public IP.')
 
-  cmd.parse(process.argv)  
+cmd.parse(process.argv)  
 
 chkip((ip) => {
   console.log(ip);
